71f33100ff5ba8f5a29183516c1c619946bdce98
felixstenian <felixstenian@gmail.com>
2023-03-31 00:53:49 -0300

    chore: starting project structure

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -40,5 +40,47 @@ module.exports = function (plop) {
         templateFile: 'templates/addIndexComponent.ts.hbs'
       }
     ]
-  })
+  }),
+    plop.setGenerator('page', {
+      description: 'application page logic',
+      prompts: [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'component name please'
+        }
+      ],
+      actions: [
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/index.ts',
+          templateFile: 'templates/index.ts.hbs'
+        },
+        {
+          type: 'add',
+          path: '../src/pages/{{pascalCase name}}/index.ts',
+          templateFile: 'templates/pageIndex.ts.hbs'
+        },
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+          templateFile: 'templates/component.tsx.hbs'
+        },
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/styles.ts',
+          templateFile: 'templates/styles.ts.hbs'
+        },
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+          templateFile: 'templates/test.tsx.hbs'
+        },
+        {
+          type: 'append',
+          path: '../src/components/index.ts',
+          templateFile: 'templates/addIndexComponent.ts.hbs'
+        }
+      ]
+    })
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Main } from './Main'
+
+export { default as Episodios } from './Episodios'
