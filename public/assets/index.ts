387a583299741290da1e51ae6a5fe92bb6deb603
felixstenian <felixstenian@gmail.com>
2023-04-03 14:06:47 -0300

    chore: add images

---
+++ public/assets/index.ts
@@ -0,0 +1,6 @@
+import background from './background-night-starry.jpg'
+import brand from './Rick_and_Morty-logo.png'
+import image_portal_and_brand from './rick-and-morty-portal.png'
+import image_portal from './rick-and-morty-image.png'
+
+export { background, brand, image_portal, image_portal_and_brand }
